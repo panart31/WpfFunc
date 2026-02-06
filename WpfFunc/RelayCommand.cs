27692ae29839f849bc4f99b16ec1cf4eeac4d475
Panart <artem-panov-2006@bk.ru>
2026-02-06 20:15:50 +0300

    Р РµР°Р»РёР·РѕРІР°РЅ RelayCommand РґР»СЏ РІС‹РїРѕР»РЅРµРЅРёСЏ РєРѕРјР°РЅРґ Р±РµР· РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ РІРЅРµС€РЅРёС… Р±РёР±Р»РёРѕС‚РµРє

--- WpfFunc/RelayCommand.cs
+++ WpfFunc/RelayCommand.cs
@@ -3,25 +3,30 @@ using System.Windows.Input;
 
 namespace WpfFunc
 {
+    // Реализация ICommand для выполнения команд в MVVM без использования библиотек
     public class RelayCommand : ICommand
     {
-        private readonly Action _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly Action _execute; // Действие для выполнения
+        private readonly Func<bool> _canExecute; // Условие возможности выполнения
 
+        // Событие изменения возможности выполнения команды
         public event EventHandler CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
 
+        // Конструктор команды с обязательным действием и опциональным условием
         public RelayCommand(Action execute, Func<bool> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
+        // Проверка возможности выполнения команды
         public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
 
+        // Выполнение команды
         public void Execute(object parameter) => _execute();
     }
 }
--- WpfFunc/ViewModelBase.cs
+++ WpfFunc/ViewModelBase.cs
@@ -3,15 +3,19 @@ using System.Runtime.CompilerServices;
 
 namespace WpfFunc
 {
+    // Базовый класс для ViewModel с ручной реализацией INotifyPropertyChanged
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        // Событие для уведомления об изменении свойств
         public event PropertyChangedEventHandler PropertyChanged;
 
+        // Вызов события изменения свойства с автоматическим определением имени через CallerMemberName
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        // Универсальный метод установки значения с проверкой на изменение и уведомлением
         protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
         {
             if (System.Collections.Generic.EqualityComparer<T>.Default.Equals(field, value))

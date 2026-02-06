d95608b42a8a9b48546746e25befe60c70f7cc67
Panart <artem-panov-2006@bk.ru>
2026-02-06 20:43:24 +0300

    РЈР»СѓС‡С€РµРЅР° РґРѕРєСѓРјРµРЅС‚Р°С†РёСЏ ViewModelBase СЃ XML-РєРѕРјРјРµРЅС‚Р°СЂРёСЏРјРё

--- WpfFunc/ViewModelBase.cs
+++ WpfFunc/ViewModelBase.cs
@@ -3,19 +3,36 @@ using System.Runtime.CompilerServices;
 
 namespace WpfFunc
 {
-    // Базовый класс для ViewModel с ручной реализацией INotifyPropertyChanged
+    /// <summary>
+    /// Базовый класс для ViewModel с ручной реализацией INotifyPropertyChanged.
+    /// Предоставляет механизм уведомления об изменении свойств для привязки данных в WPF.
+    /// </summary>
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
-        // Событие для уведомления об изменении свойств
+        /// <summary>
+        /// Событие для уведомления об изменении свойств.
+        /// Используется механизмом привязки данных WPF для автоматического обновления UI.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
-        // Вызов события изменения свойства с автоматическим определением имени через CallerMemberName
+        /// <summary>
+        /// Вызывает событие изменения свойства с автоматическим определением имени через CallerMemberName.
+        /// </summary>
+        /// <param name="propertyName">Имя изменённого свойства (определяется автоматически)</param>
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        // Универсальный метод установки значения с проверкой на изменение и уведомлением
+        /// <summary>
+        /// Универсальный метод установки значения с проверкой на изменение и уведомлением.
+        /// Предотвращает лишние обновления UI при установке того же значения.
+        /// </summary>
+        /// <typeparam name="T">Тип значения свойства</typeparam>
+        /// <param name="field">Поле для хранения значения</param>
+        /// <param name="value">Новое значение</param>
+        /// <param name="propertyName">Имя свойства (определяется автоматически)</param>
+        /// <returns>true, если значение было изменено, иначе false</returns>
         protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
         {
             if (System.Collections.Generic.EqualityComparer<T>.Default.Equals(field, value))

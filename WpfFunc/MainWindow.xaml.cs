da905523379c0470c56bb28fe2e36c4bde5647a9
Panart <artem-panov-2006@bk.ru>
2026-02-06 20:16:39 +0300

    Update MainWindow to use CommunityToolkit-based ViewModel

--- WpfFunc/MainWindow.xaml.cs
+++ WpfFunc/MainWindow.xaml.cs
@@ -2,11 +2,13 @@
 
 namespace WpfFunc
 {
+    // Главное окно с установкой DataContext для привязки данных через CommunityToolkit.MVVM
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            // Установка ViewModel как DataContext - ViewModel использует ObservableObject из библиотеки
             DataContext = new MainViewModel();
         }
     }
